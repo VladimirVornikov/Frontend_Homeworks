97198e7436e04c726d8145eef44cd58ac877effa
VladimirVornikov <vornikov-vladimir@mail.ru>
2023-06-14 12:09:04 +0200

    14.06.2023

--- Homework№7/script.js
+++ Homework№7/script.js
@@ -1,34 +1,45 @@
-// //1. Напишите функцию, которая принимает два числовых аргумента и выводит в консоль все четные числа от большего
-// //  к меньшему.
-function evenFromMaxToMin(num1, num2) {
-  for (let i = Math.max(num1, num2); i >= Math.min(num1, num2); i--) {
-    if (i % 2 == 0) {
-      console.log(i);
-    }
-  }
-}
-evenFromMaxToMin(11, 21);
+// // //1. Напишите функцию, которая принимает два числовых аргумента и выводит в консоль все четные числа от большего
+// // //  к меньшему.
+// function evenFromMaxToMin(num1, num2) {
+//   for (let i = Math.max(num1, num2); i >= Math.min(num1, num2); i--) {
+//     if (i % 2 == 0) {
+//       console.log(i);
+//     }
+//   }
+// }
+// evenFromMaxToMin(11, 21);
 
-// //2. Напишите функцию power, которая принимает два числовых аргумента (основание степени и саму степень) и возвращает
-// // число в указанной степени. Значение степени должно быть указано по умолчанию 2
+// // //2. Напишите функцию power, которая принимает два числовых аргумента (основание степени и саму степень) и возвращает
+// // // число в указанной степени. Значение степени должно быть указано по умолчанию 2
 
-function power(num, pow = 2) {
-  return num ** pow;
-}
-console.log(power(3));
+// function power(num, pow = 2) {
+//   return num ** pow;
+// }
+// console.log(power(3));
 
-// 3. Напишите функцию, которая принимает числовой аргумент n и считает сумму чисел от 1 до n.
-function sumOfN_Numers(num) {
-  let sum = 0;
-  for (let i = 1; i <= num; i++) {
-    sum += i;
-  }
-  return sum;
-}
-console.log(sumOfN_Numers(4));
+// // 3. Напишите функцию, которая принимает числовой аргумент n и считает сумму чисел от 1 до n.
+// function sumOfN_Numers(num) {
+//   let sum = 0;
+//   for (let i = 1; i <= num; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+// console.log(sumOfN_Numers(4));
 
 // // 4. Напишите функцию, которая принимает два числовых аргумента n и m и считает сумму четных чисел и нечетных
 //  чисел от n до m. Суммы выведите в консоль (в начале сумму четных чисел, а затем сумму нечетных).
-function SumOfOddAndEven(num1, num2) {
-    
+function SumOfOddAndEven(n, m) {
+  let oddSum = 0;
+  let evenSum = 0;
+  for (let i = n; i < m; i++) {
+    if (i % 2 == 0) {
+      evenSum += i;
+    } else {
+      oddSum += i;
+    }
+  }
+  console.log(evenSum);
+  console.log(oddSum);
 }
+SumOfOddAndEven(2, 5)
